153c8c89469badd0572496e9c545732df68a1e8e
starkid111 <ramadanadex111@gmail.com>
2025-10-06 17:44:57 +0100

    minor fix

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -46,9 +46,9 @@ export default function Projects() {
       viewport={{ once: true }}
       transition={{ duration: 0.8 }}
       className="py-20 px-6"
-      data-aos="fade-up"
+
     >
-      <div className="max-w-5xl mx-auto">
+      <div className="max-w-5xl mx-auto" data-aos="fade-up">
         <h2 className="text-3xl font-bold text-center text-cyan-700 mb-12 tracking-widest">Projects.</h2>
         <div className="grid md:grid-cols-3 gap-8">
           {projects.map((project, i) => (
@@ -60,6 +60,7 @@ export default function Projects() {
               transition={{ duration: 0.6, delay: i * 0.2 }}
               className="rounded-2xl shadow p-6 hover:shadow-lg transition"
               style={{ backgroundImage: "url('/second-bg.svg')" }}
+              data-aos="fade-up"
             >
                <Image src={project.src} width={400} height={200} alt={project.title} className="rounded-lg mb-4"/>
               <h3 className="text-xl font-semibold text-gray-600  mb-2">{project.title}</h3>
