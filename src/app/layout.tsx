1bb8c02368e2c1d17b1a8882fc7ea363a2824542
starkid111 <ramadanadex111@gmail.com>
2025-11-24 12:13:06 +0100

    minor UI fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,9 +27,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-       <head>
-        <link rel="preload" href="" />
-       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         style={{ backgroundImage: "url('/portfolio-bg.jpg')" }}
